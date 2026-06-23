710f47df2478a677870d7edf3a00126b38f73f46
Bhavaniprasad7 <bhanu23237@gmail.com>
2026-06-23 15:01:25 +0530

    chnaged backend

--- terraform/DEV/backend.tf
+++ terraform/DEV/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG-AIS-DEV-ORP"
-    storage_account_name = "saaisdevorp"
+    resource_group_name  = "RRG-AIS-DEV-TFSTATE"
+    storage_account_name = "saasidevtf"
     container_name       = "terraform-state-file"
     key                  = "dev.terraform.tfstate"
   }

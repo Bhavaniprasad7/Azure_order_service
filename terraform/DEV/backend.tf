28c1297c85d71968fdbe43fb56cee16b72cb829a
Bhavaniprasad7 <bhanu23237@gmail.com>
2026-06-23 15:04:37 +0530

    backebd

--- terraform/DEV/backend.tf
+++ terraform/DEV/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RRG-AIS-DEV-TFSTATE"
+    resource_group_name  = "RG-AIS-DEV-TFSTATE"
     storage_account_name = "saasidevtf"
     container_name       = "terraform-state-file"
     key                  = "dev.terraform.tfstate"
